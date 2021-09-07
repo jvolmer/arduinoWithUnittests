38019706421630236d19ec2bd343a0a13db3dbe9
jvolmer <juliavolmer@t-online.de>
2021-09-08 01:28:09 +0200

    Compile arduino core inside

--- README.md
+++ README.md
@@ -59,17 +59,17 @@ This gives an overview on the command line and writes all information into file 
 
 
 ## Install
+### Prerequisites
 These are the prerequisites you need to install in order to build the program:
 * [Arduino-IDE](https://www.arduino.cc/en/software)
 * [Conan.io](https://docs.conan.io/en/latest/installation.html)
 * [cmake 3.13+](https://cmake.org/download/)
-* avr-g++ and avr-objcopy (an old but still relevant installation description for the full avr toolchain can be found [here](https://mightyohm.com/blog/tutorials/avr-toolchain-installation/))
+* avr-g++, avr-gcc, avr-ar and avr-objcopy (an old but still relevant installation description for the full avr toolchain can be found [here](https://mightyohm.com/blog/tutorials/avr-toolchain-installation/))
 * C++ compiler, e.g. GNU C++ Compiler g++
 
-You also need the Arduino core library, which you can create by yourself with the Arduino source you files and cmake, I will shortly create a description on how to do that.
+__Additionally, you have to set the variables in variables.cmake according to you computer, microcontroller and Arduino IDE installation specifics.__
 
-Additionally, you have to set the variables in variables.cmake according to you computer, microcontroller and Arduino IDE installation specifics.
-
+## Installation and Configuration
 Inside the project directory, install the dependencies:
 ```bash
 conan install -if build .
@@ -80,7 +80,8 @@ Configure the build system:
 cmake -S . -B build
 ```
 This project includes tests that only compile with a standard C++ compiler (e.g. g++) and the executable that only compiles with the avr-g++ compiler, therefore depending what you want to do, you have to set the following variables when configuring the build system, add the following to the line above:
-* for executable to compile use `-D CMAKE_CXX_COMPILER=avr-g++`
+* for executable to compile add `-D CMAKE_CXX_COMPILER=avr-g++`
 * for test to compile add `-D CMAKE_CXX_COMPILER=g++`
-* for measuring coverage use `-D CMAKE_CXX_COMPILE=g++ -D CMAKE_BUILD_TYPE=Debug`
+* for measuring coverage add `-D CMAKE_CXX_COMPILE=g++ -D CMAKE_BUILD_TYPE=Debug`
+
 Have a look at the Usage section for a more detailed explanation how and when to use these variables.
--- variables.cmake
+++ variables.cmake
@@ -10,15 +10,57 @@ if (CMAKE_CXX_COMPILER MATCHES avr)
   set(UPLOAD_SPEED 115200)
 
   # depending on your Arduino IDE installation
-  set(ARDUINO_DIR /home/jvolmer/bin/arduino-1.8.10)
+  set(ARDUINO_DIR "") # set this to the installation path of the Arduino IDE
+  set(ARDUINO_CORE_DIR ${ARDUINO_DIR}/hardware/arduino/avr/cores/arduino)
+  list(APPEND ARDUINO_CORE_SRCS
+    ${ARDUINO_CORE_DIR}/abi.cpp
+    ${ARDUINO_CORE_DIR}/Arduino.h
+    ${ARDUINO_CORE_DIR}/binary.h
+    ${ARDUINO_CORE_DIR}/CDC.cpp
+    ${ARDUINO_CORE_DIR}/Client.h
+    ${ARDUINO_CORE_DIR}/HardwareSerial0.cpp
+    ${ARDUINO_CORE_DIR}/HardwareSerial1.cpp
+    ${ARDUINO_CORE_DIR}/HardwareSerial2.cpp
+    ${ARDUINO_CORE_DIR}/HardwareSerial3.cpp
+    ${ARDUINO_CORE_DIR}/HardwareSerial.cpp
+    ${ARDUINO_CORE_DIR}/HardwareSerial.h
+    ${ARDUINO_CORE_DIR}/HardwareSerial_private.h
+    ${ARDUINO_CORE_DIR}/hooks.c
+    ${ARDUINO_CORE_DIR}/IPAddress.cpp
+    ${ARDUINO_CORE_DIR}/IPAddress.h
+    ${ARDUINO_CORE_DIR}/main.cpp
+    ${ARDUINO_CORE_DIR}/new.cpp
+    ${ARDUINO_CORE_DIR}/new.h
+    ${ARDUINO_CORE_DIR}/PluggableUSB.cpp
+    ${ARDUINO_CORE_DIR}/PluggableUSB.h
+    ${ARDUINO_CORE_DIR}/Printable.h
+    ${ARDUINO_CORE_DIR}/Print.cpp
+    ${ARDUINO_CORE_DIR}/Print.h
+    ${ARDUINO_CORE_DIR}/Server.h
+    ${ARDUINO_CORE_DIR}/Stream.cpp
+    ${ARDUINO_CORE_DIR}/Stream.h
+    ${ARDUINO_CORE_DIR}/Tone.cpp
+    ${ARDUINO_CORE_DIR}/Udp.h
+    ${ARDUINO_CORE_DIR}/USBAPI.h
+    ${ARDUINO_CORE_DIR}/USBCore.cpp
+    ${ARDUINO_CORE_DIR}/USBCore.h
+    ${ARDUINO_CORE_DIR}/USBDesc.h
+    ${ARDUINO_CORE_DIR}/WCharacter.h
+    ${ARDUINO_CORE_DIR}/WInterrupts.c
+    ${ARDUINO_CORE_DIR}/wiring_analog.c
+    ${ARDUINO_CORE_DIR}/wiring.c
+    ${ARDUINO_CORE_DIR}/wiring_digital.c
+    ${ARDUINO_CORE_DIR}/wiring_private.h
+    ${ARDUINO_CORE_DIR}/wiring_pulse.c
+    ${ARDUINO_CORE_DIR}/wiring_shift.c
+    ${ARDUINO_CORE_DIR}/WMath.cpp
+    ${ARDUINO_CORE_DIR}/WString.cpp
+    ${ARDUINO_CORE_DIR}/WString.h)
   set(ARDUINO_INCLUDES
-    ${ARDUINO_DIR}/hardware/arduino/avr/cores/arduino/;
+    ${ARDUINO_CORE_DIR}/;
     ${ARDUINO_DIR}/hardware/arduino/avr/variants/standard/)
   # avrdude version must be at least from 2019, is given for version from Arduino IDE
   set(AVRDUDE ${ARDUINO_DIR}/hardware/tools/avr/bin/avrdude)
   set(ARVDUDE_CONF ${ARDUINO_DIR}/hardware/tools/avr/etc/avrdude.conf)
-
-  # self-compiled library from the Arduino source files
-  set(ARDUINO_LIB /home/jvolmer/lib/arduino/coreObjectFiles/libcore.a)
-
+  
 endif()
